4311cd9958efa845f13796ecbe36604f6ab9b446
Ahmed Hussien <ahussien.gomaa@gmail.com>
2020-07-22 14:16:39 +0400

    Fixing test issue

--- product-service/src/test/java/com/dsc/indices/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/dsc/indices/productservice/ProductServiceApplicationTests.java
@@ -2,12 +2,19 @@ package com.dsc.indices.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootTest
+@ComponentScan("se.magnus")
 class ProductServiceApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
 
+	@Configuration
+	@ComponentScan("se.magnus")
+	public static class SpringConfig {
+
+	}
 }
